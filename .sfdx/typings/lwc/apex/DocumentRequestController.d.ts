61e0def74b26999f14ff2c6762f285012687c9fe
Alexia Abrego <Alexia.Abrego@trinity-crm.com>
2025-11-14 08:51:06 -0700

    Updating documents to support multi claims

--- .sfdx/typings/lwc/apex/DocumentRequestController.d.ts
+++ .sfdx/typings/lwc/apex/DocumentRequestController.d.ts
@@ -28,69 +28,9 @@ declare module "@salesforce/apex/DocumentRequestController.updateAgentFields" {
 declare module "@salesforce/apex/DocumentRequestController.mergeAndReturnFile" {
   export default function mergeAndReturnFile(param: {mappingId: any, recordId: any, objectApiName: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DocumentRequestController.setCasePeriodDates" {
-  export default function setCasePeriodDates(param: {caseId: any, dateStart: any, dateEnd: any}): Promise<any>;
+declare module "@salesforce/apex/DocumentRequestController.fetchClaims" {
+  export default function fetchClaims(param: {parentId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DocumentRequestController.clearCasePeriodDates" {
-  export default function clearCasePeriodDates(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseAnnuity" {
-  export default function setCaseAnnuity(param: {caseId: any, annuity: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseFundingChange" {
-  export default function setCaseFundingChange(param: {caseId: any, fundingChange: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.fetchPayeeAddresses" {
-  export default function fetchPayeeAddresses(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.fetchAccountAddresses" {
-  export default function fetchAccountAddresses(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseAddress" {
-  export default function setCaseAddress(param: {caseId: any, address: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseInsertInformation" {
-  export default function setCaseInsertInformation(param: {caseId: any, insertInformation: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.searchCodes" {
-  export default function searchCodes(param: {term: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseInjuredDiagnosis" {
-  export default function setCaseInjuredDiagnosis(param: {caseId: any, codeId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseWithdrawalExtras" {
-  export default function setCaseWithdrawalExtras(param: {caseId: any, carrierOrInsurance: any, allocationCompany: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setSpecifyReason" {
-  export default function setSpecifyReason(param: {caseId: any, insertInformation: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setDocumentationRequest" {
-  export default function setDocumentationRequest(param: {caseId: any, insertInformation: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setProviderAttention" {
-  export default function setProviderAttention(param: {caseId: any, providerAttention: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseNextAnnuityDate" {
-  export default function setCaseNextAnnuityDate(param: {caseId: any, insertDate: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.fetchExhaustionsForCase" {
-  export default function fetchExhaustionsForCase(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseExhaustion" {
-  export default function setCaseExhaustion(param: {caseId: any, exhaustionId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.validateBELAdmin" {
-  export default function validateBELAdmin(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.updateAdministrationAddress" {
-  export default function updateAdministrationAddress(param: {adminId: any, street: any, city: any, state: any, postalCode: any, countryCodeOpt: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.getCountryCodeToLabelMap" {
-  export default function getCountryCodeToLabelMap(): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.getStateCodeToLabelMap" {
-  export default function getStateCodeToLabelMap(param: {countryCode: any}): Promise<any>;
-}
-declare module "@salesforce/apex/DocumentRequestController.setCaseLRDates" {
-  export default function setCaseLRDates(param: {caseId: any, dateIssued: any, datePriorIssued: any}): Promise<any>;
+declare module "@salesforce/apex/DocumentRequestController.updateSelectedClaims" {
+  export default function updateSelectedClaims(param: {recordId: any, claimIds: any}): Promise<any>;
 }
